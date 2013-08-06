841f5616b1703ed0ba264e83adf7899e8e94a572
Jeremie <taboada.jeremie@gmail.com>
2013-08-07 01:13:44 +0200

    Update Game to spawn more asteroides

--- scripts/game/AsteroideEmitter.js
+++ scripts/game/AsteroideEmitter.js
@@ -4,33 +4,44 @@
 function AsteroideEmitter(scene) {
   this.scene = scene;
   this.spawnTime = 1;
-  this.currentTime = 0;
-  this.asteroides = [];
+  this._currentTime = 0;
+  this._spawnTimeUpdate = 0;
+  this._asteroides = [];
+}
+
+var _updateSpawnTime = function (self, deltaTime) {
+  if (self._spawnTimeUpdate < 20 * 1000)
+    self._spawnTimeUpdate += deltaTime;
+  else if (self.spawnTime > 0.05) {
+    self._spawnTimeUpdate = 0;
+    self.spawnTime /= 2;
+  }
 }
 
 AsteroideEmitter.prototype.update = function (deltaTime) {
-  if (this.currentTime < this.spawnTime * 1000)
-    this.currentTime += deltaTime;
+  _updateSpawnTime(this, deltaTime);
+  if (this._currentTime < this.spawnTime * 1000)
+    this._currentTime += deltaTime;
   else {
-    this.currentTime = 0;
+    this._currentTime = 0;
     var a = new Asteroide(100, 100);
-    this.asteroides.push(a);
+    this._asteroides.push(a);
     this.scene.add(a.mesh);
   }
-  for (var i = this.asteroides.length - 1; i >= 0; --i) {
-    if (this.asteroides[i].mesh.position.z > 10) {
-      this.scene.remove(this.asteroides[i].mesh);
-      this.asteroides.splice(i, 1);
+  for (var i = this._asteroides.length - 1; i >= 0; --i) {
+    if (this._asteroides[i].mesh.position.z > 10) {
+      this.scene.remove(this._asteroides[i].mesh);
+      this._asteroides.splice(i, 1);
     } else
-      this.asteroides[i].update(deltaTime);
+      this._asteroides[i].update(deltaTime);
   }
 }
 
 AsteroideEmitter.prototype.hit = function (target) {
-  for (var i = this.asteroides.length - 1; i >= 0; --i) {
-    if (this.asteroides[i].hit(target)) {
-      this.scene.remove(this.asteroides[i].mesh);
-      this.asteroides.splice(i, 1);
+  for (var i = this._asteroides.length - 1; i >= 0; --i) {
+    if (this._asteroides[i].hit(target)) {
+      this.scene.remove(this._asteroides[i].mesh);
+      this._asteroides.splice(i, 1);
       return true;
     }
   }
