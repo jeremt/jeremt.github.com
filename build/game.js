4258ac911dbd3526dc0e125e91947d6d1ef78997
Jeremie <taboada.jeremie@gmail.com>
2013-08-20 14:43:04 +0200

    Update comments style

--- build/game.js
+++ build/game.js
@@ -3980,12 +3980,12 @@ window.Hud = Hud;
  * The core is a global which store all game informations.
  */
 var Core = {
-  width   : 100,    //< The width of the area.
-  height  : 100,    //< The height of the area.
-  depth   : 500,    //< The depth of the area.
-  score   : 0,      //< Player's score.
-  lifes   : 5,      //< Player's lifes.
-  highQuality: true //< Graphic quality level.
+  width        : 100,   /** The width of the area. */
+  height       : 100,   /** The height of the area. */
+  depth        : 500,   /** The depth of the area. */
+  score        : 0,     /** Player's score. */
+  lifes        : 5,     /** Player's lifes. */
+  highQuality  : true   /** Graphic quality level. */
 };
 ~function () {
 
