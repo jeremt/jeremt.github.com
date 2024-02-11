ba6971c9b882d1624db211b5eaa94daee3e2c5e0
jeremt <taboada.jeremie@gmail.com>
2024-02-11 16:26:49 +0100

    Add some comments in JS

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
-// 3D Stuffs
+// Init the canvas, scene, etc.
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 const container = document.querySelector('#thinkingHead');
-const {width, height} = container.getBoundingClientRect();
+const {width, height} = container.getBoundingClientRect(); // use the actual size of the canvas for the renderer
 
 const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.toneMapping = THREE.ACESFilmicToneMapping; // better colors
+renderer.setPixelRatio(window.devicePixelRatio); // better resolution
 renderer.setSize(width, height);
 
 container.appendChild(renderer.domElement);
+
+addEventListener('resize', () => {
+    const {width, height} = container.getBoundingClientRect();
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+});
+
+// Add some lights
 
 const dirLight = new THREE.DirectionalLight(0xffffff);
 dirLight.position.x = 0.5;
@@ -28,17 +37,21 @@ scene.add(frontDirLight);
 const light = new THREE.AmbientLight(0xffffff, 2);
 scene.add(light);
 
+// Move the camera to better see the model
+
 camera.position.y = -0.05;
 camera.position.z = 0.75;
 
+// Load the model
+
 const loader = new GLTFLoader();
-let model = null;
+let model = null; // we store the model globally so we can update its rotation later
 
 loader.load(
     '/assets/thinking_emoji.glb',
     function (gltf) {
+        // store the model and add it to the scene
         model = gltf.scene;
-        console.log(gltf.scene);
         scene.add(gltf.scene);
         renderer.render(scene, camera);
     },
@@ -49,21 +62,16 @@ loader.load(
 );
 
 addEventListener('mousemove', (event) => {
+    // if the model is loaded, we update its rotation according to the mouse position
     if (model) {
         model.rotation.x = event.clientY / innerHeight - 0.1; // easier to look down than up 😅
         model.rotation.y = event.clientX / innerWidth - 0.5;
+        // ps: we divide the position by innerWidth to have a normalized value (between 0 and 1 instead of pixels)
+        renderer.render(scene, camera);
     }
-    renderer.render(scene, camera);
 });
 
-addEventListener('resize', () => {
-    const {width, height} = container.getBoundingClientRect();
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(width, height);
-});
-
-// Dark mode
+// Handle switch between light and dark mode
 
 const media = matchMedia('(prefers-color-scheme: dark)');
 media.addEventListener('change', applyTheme);
